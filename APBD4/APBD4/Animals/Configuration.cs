cba99295aeda68f96c4c78177bc22e0995b9a4cb
CornifersWife <irys.latosz@gmail.com>
2024-04-11 16:28:43 +0200

    finished implementing functionality

---
+++ APBD4/APBD4/Animals/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using APBD4.Visits;
+namespace APBD4.Animals;
+
+public static class Configuration
+{
+    public static void RegisterEndpointsForAnimals(this IEndpointRouteBuilder app) {
+        app.MapGet("/api/v1/animals", (IAnimalService service) => {
+            return TypedResults.Ok(service.GetAnimals());
+        });
+        
+        app.MapGet("/api/v1/animals/{id::int}", (int id,IAnimalService service) => {
+            return TypedResults.Ok(service.GetAnimal(id));
+        });
+        
+        app.MapPut("/api/v1/animals", (Animal newAnimal,IAnimalService service) => {
+            service.AddAnimal(newAnimal);
+            return TypedResults.Ok("Animal added");
+        });
+        app.MapDelete("/api/v1/animals", (int id,IAnimalService service) => {
+            Animal animalToRemove = service.GetAnimal(id);
+            service.RemoveAnimal(animalToRemove);
+            return TypedResults.Ok("Animal removed");
+        });
+        app.MapPost("/api/v1/animals", (Animal newAnimal,IAnimalService service) => {
+            Animal oldAnimal = service.GetAnimal(newAnimal.Id);
+            service.ChangeAnimalInfo(oldAnimal,newAnimal);
+            return TypedResults.Ok("Animal info changed");
+        });
+        
+        
+        app.MapGet("/api/v1/animals/visits", (Animal animal,IVisitService service) =>
+        {
+            return TypedResults.Ok(service.GetAnimalVisits(animal));
+        });
+        app.MapPut("/api/v1/animals/visits", (Visit newVisit,IVisitService service) =>
+        {
+            service.AddNewVisit(newVisit);
+            return TypedResults.Ok();
+        });
+    }
+}
---
+++ APBD4/APBD4/Visits/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using APBD4.Animals;
+
+namespace APBD4.Visits;
+
+public static class Configuration {
+    public static void RegisterEndpointsForVisits(this IEndpointRouteBuilder app) {
+        app.MapGet("/api/v1/visits", (Animal animal, IVisitService service) => {
+                return TypedResults.Ok(service.GetAnimalVisits(animal));
+            });
+        app.MapPut("/api/v1/visits", (Visit newVisit, IVisitService service) => {
+            service.AddNewVisit(newVisit);
+            return TypedResults.Ok();
+        });
+    }
+}
